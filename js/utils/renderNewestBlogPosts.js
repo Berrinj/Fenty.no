34fd23b0be7d1fd1c9b6d0043772434b27bd2b58
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-10-17 13:46:26 +0200

    js github.io problem, no fetch renders

--- js/utils/renderNewestBlogPosts.js
+++ js/utils/renderNewestBlogPosts.js
@@ -8,16 +8,15 @@ export async function renderNewestBlogPosts() {
   try {
     loader.style.display = "none";
     let newPostList = await getPosts(FENTY_EMBED_API_URL);
+    console.log(newPostList);
     const postItemsHTML = newPostList.slice(0, 8).map((newPostItem) => {
       return `<li><a href="single-post.html?id=${newPostItem.id}">${newPostItem.title.rendered}<p class="go-to-post">Les mer..</p></li></a></li>`;
     });
 
     newestBlogPostSection.innerHTML = postItemsHTML.join("");
-
-    const loaderElement = document.querySelector(".loader");
-    loaderElement.remove();
   } catch (error) {
     newestBlogPostSection.innerHTML = `<div class="error">En feil oppsto ved henting av nye innlegg</div>`;
+    console.error("En feil oppsto:", error);
     throw error;
   }
 }
